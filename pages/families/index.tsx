e46ff47f859ab668b87eea50181132f47bf5235d
Alessandrim <fons.code@gmail.com>
2021-06-14 12:27:06 -0400

    Add Icon component to make more readable code

--- pages/families/index.tsx
+++ pages/families/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 // components
 import Layout from 'components/Layout'
+import Datatable from 'components/datatable'
 
 export default function FamilyPage() {
     return (
         <Layout>
             <h1>Families</h1>
+            <Datatable/>
         </Layout>
     )
 }
