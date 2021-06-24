d1d166dcadd056302bf53f8ea733523e413b8c8a
fonscode <fons.code@gmail.com>
2021-06-24 17:11:05 -0400

    add home details, map, tags molecule

---
+++ src/components/UI/Molecules/TagInput.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+//components
+import { InputText } from "primereact/inputtext";
+import { Button } from 'primereact/button';
+import Tag from 'components/UI/Atoms/Tag'
+//styles
+import classes from "styles/UI/Molecules/TagInput.module.scss"
+export default function TagInput({ placeholder, value, setValue }) {
+    const [tag, setTag] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setValue(value.concat(tag))
+        setTag('')
+    }
+    const handleDelete = index => {
+        console.log("its working", index)
+        const newArray = value.filter(item => {
+            return item !== value[index]
+        })
+        setValue(newArray)
+    }
+    return (
+        <div>
+            <form onSubmit={e => handleSubmit(e)}>
+                <InputText name='tags' value={tag} placeholder={placeholder} onChange={e => setTag(e.target.value)} />
+                <Button label="Add" />
+            </form>
+            <div className={classes.tag_container}>
+                {value.map((tag, index) => <Tag label={tag} key={index} color={`hsl(${(Math.floor(Math.random() * 36)*10)}, 60%, 84%)`} callback={() => handleDelete(index)}/>)}
+            </div>
+        </div>
+    )
+}
