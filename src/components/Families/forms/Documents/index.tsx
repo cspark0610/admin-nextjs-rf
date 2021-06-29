5e5a841ecc5551094033de9b7aa6e9fa4052eaeb
fonscode <fons.code@gmail.com>
2021-06-29 12:10:09 -0400

    add review templates, change avatar img, fix styles

--- src/components/Families/forms/Documents/index.tsx
+++ src/components/Families/forms/Documents/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+//components
+import Table from 'components/UI/Organism/Table'
 
 export default function DocumentsForm() {
+    
+    const data= [{name:'Performance', description:'this is a doc', url:'https://wwww.docs.google.com'},{name:'Admin', description:'this is a doc', url:'https://wwww.docs.google.com'},{name:'Revenue', description:'this is a doc', url:'https://wwww.docs.google.com'}]
+    const docsColumns = [
+        {field: 'name', header: 'Name', filterPlaceholder: 'Search by name'},
+        {field: 'description', header: 'Description', filterPlaceholder: 'Search by description'},
+        {field: 'url', header: 'Url', filterPlaceholder: 'Search by url'},
+    ]
     return (
         <div>
-            Documents
+            <h1>Documents</h1>
+            <Table name='Documents' columns={docsColumns} content={data} />
         </div>
     )
 }
