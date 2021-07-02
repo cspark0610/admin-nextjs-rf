136d0349e346d43dde4b0eb13cde8a6aef049add
fonscode <fons.code@gmail.com>
2021-07-02 12:45:44 -0400

    add family tab

--- src/components/Families/forms/Family/index.tsx
+++ src/components/Families/forms/Family/index.tsx
@@ -1,9 +1,227 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+//components
+import FormGroup from 'components/UI/Molecules/FormGroup'
+import FormHeader from 'components/UI/Molecules/FormHeader'
+import { Panel } from 'primereact/panel';
+import InputContainer from 'components/UI/Molecules/InputContainer'
+import { MultiSelect } from "primereact/multiselect";
+import { InputText } from "primereact/inputtext";
+import { FileUpload } from 'primereact/fileupload';
+import { InputTextarea } from 'primereact/inputtextarea';
+import Table from 'components/UI/Organism/Table'
+//styles
+import classes from 'styles/Families/Forms.module.scss'
+//Context
+import { FamilyContext } from 'context/FamilyContext'
 
 export default function FamilyForm() {
+    const { family } = useContext(FamilyContext)
+    const [rules, setRules] = useState([])
+    const [localCoordinator, setLocalCoordinator] = useState('')
+    const [welcomeLetter, setWelcomeLetter] = useState(family.welcomeLetter)
+    //data fot datatables
+    
+    const familyMembers = family.familyMembers.map(({ firstName, lastName, birthDate, gender }) => {
+        return (
+            {
+                firstName,
+                lastName,
+                birthDate,
+                age: 17,
+                gender
+            }
+        )
+    })
+    const pets = family.pets.map(({name, age, race, remarks, type}) => {
+        return(
+            {
+                name,
+                age,
+                breed: race,
+                remarks,
+                type
+            }
+        )
+    })
+    const externalStudents = []
+    const tenants = []
+    const schools = []
+    const genders= ['Male', 'Female', 'Other', 'No binary']
+    //columns for datatables
+
+    const familyMembersColumn = [
+        {
+            field: "firstName",
+            header: "FirstName",
+            filterPlaceholder: "Search by firstName"
+        },
+        {
+            field: "lastName",
+            header: "LastName",
+            filterPlaceholder: "Search by lastName"
+        },
+        {
+            field: "birthDate",
+            header: "birth date",
+            filterPlaceholder: "Search by birth date"
+        },
+        {
+            field: "age",
+            header: "Age",
+            filterPlaceholder: "Search by age"
+        },
+        {
+            field: "gender",
+            header: "gender",
+            filterPlaceholder: "Search by gender"
+        },
+    ]
+    const petsColumns = [
+        {
+            field: "name",
+            header: "Name",
+            filterPlaceholder: "Search by name"
+        },
+        {
+            field: "age",
+            header: "Age",
+            filterPlaceholder: "Search by age"
+        },
+        {
+            field: "type",
+            header: "Type",
+            filterPlaceholder: "Search by type"
+        },
+        {
+            field: "breed",
+            header: "Breed",
+            filterPlaceholder: "Search by breed"
+        },
+        {
+            field: "remarks",
+            header: "remarks",
+            filterPlaceholder: "Search by remarks"
+        },
+    ]
+    const externalStudentsColumns = [
+        {
+            field: "name",
+            header: "Name",
+            filterPlaceholder: "Search by name"
+        },
+        {
+            field: "nationality",
+            header: "Nationality",
+            filterPlaceholder: "Search by nationality"
+        },
+        {
+            field: "gender",
+            header: "gender",
+            filterPlaceholder: "Search by gender"
+        },
+        {
+            field: "age",
+            header: "Age",
+            filterPlaceholder: "Search by age"
+        },
+        {
+            field: "length",
+            header: "Length to stay",
+            filterPlaceholder: "Search by interval"
+        },
+    ]
+    const tenantsColumns = [
+        {
+            field: "name",
+            header: "Name",
+            filterPlaceholder: "Search by name"
+        },
+        {
+            field: "age",
+            header: "Age",
+            filterPlaceholder: "Search by age"
+        },
+        {
+            field: "gender",
+            header: "Gender",
+            filterPlaceholder: "Search by gender"
+        },
+    ]
+    const schoolsColumns = [
+        {
+            field: "school",
+            header: "School/College",
+            filterPlaceholder: "Search by name"
+        },
+        {
+            field: "course",
+            header: "Course",
+            filterPlaceholder: "Search by course"
+        },
+        {
+            field: "type",
+            header: "Type",
+            filterPlaceholder: "Search by type"
+        }, 
+    ]
+    const rulesList = ['No smoke', 'No drink alcohol']
+    const handleSubmit = (e) => {
+        e.preventdefault()
+    }
     return (
-        <div>
-            Family form
-        </div>
+        <form onSubmit={e => { handleSubmit(e) }}>
+            <FormHeader title="Family" />
+            <FormGroup title='Welcome'>
+            <div className={classes.form_container_multiple}>
+                <InputContainer label='Welcome video'>
+                    <video width="100%" height="auto" controls>
+                        <source src={family.video} type="video/mp4"/>
+                        Your browser does not support the video tag.
+                    </video>
+                    <div style={{display: 'flex', justifyContent:'space-between', alignItems: 'center', marginTop: '1em'}}>
+                        <p>Add new welcome video</p>
+                        <FileUpload mode="basic" name="welcomeVideo" url="https://primefaces.org/primereact/showcase/upload.php" accept="video/*" maxFileSize={1000000} />
+                    </div>
+                </InputContainer>
+                <div>
+                    <InputContainer label="Welcome letter">
+                    <InputTextarea  rows={5} cols={30} autoResize value={welcomeLetter} onChange={e => {setWelcomeLetter(e.target.value)}} />
+                    </InputContainer>
+                    <div>
+                        <p>This family is receiving: </p>
+                        <InputContainer label="Genders">
+                            <MultiSelect placeholder="Select gender" options={genders} />
+                        </InputContainer>
+                        
+                    </div>
+                </div>
+            </div>
+            </FormGroup>
+            <FormGroup title="Family">
+                <Panel header="Members of the family" toggleable>
+                    <Table name="Family members" columns={familyMembersColumn} content={familyMembers}/>
+                </Panel>
+                <Panel header="Pets" toggleable style={{marginTop: '3rem'}}>
+                    <Table name="Pets" columns={petsColumns} content={pets}/>
+                </Panel>
+                <Panel header="External Students" toggleable style={{marginTop: '3rem'}}>
+                    <Table name="External Students" columns={externalStudentsColumns} content={externalStudents}/>
+                </Panel>
+                <Panel header="Tenants" toggleable style={{marginTop: '3rem'}}>
+                    <Table name="Tenants" columns={tenantsColumns} content={tenants}/>
+                </Panel>
+            </FormGroup>
+            <FormGroup title='Rules'>
+                <InputContainer label='Rules'>
+                    <MultiSelect options={rulesList} value={rules} onChange={e => {setRules(e.target.value)}} placeholder="Select a rule"/>
+                </InputContainer>
+                <InputContainer label='Local Coordinator'>
+                    <InputText placeholder="Local coordinator" value={localCoordinator} onChange={e => {setLocalCoordinator(e.target.value)}}></InputText>
+                </InputContainer>         
+            </FormGroup>
+            <FormGroup title="Schools">
+                <Table name="Schools" columns={schoolsColumns} content={schools} />
+            </FormGroup>
+        </form>
     )
 }
--- src/components/UI/Organism/Table.tsx
+++ src/components/UI/Organism/Table.tsx
@@ -13,78 +13,83 @@ import classes from "styles/Families/Datatable.module.scss";
 //     filterPlaceholder: string
 // }
 interface Props {
-    name:string,
-    content: any,
-    columns: any
+  name: string,
+  content: any,
+  columns: any
 }
-const Table : React.FC<Props> = ({name,content,columns}) => {
-    const [globalFilter, setGlobalFilter] = useState("");
-    const [selectedContent,SetSelectedContent] = useState(null)
-    const dt = useRef()
-    const editItem = (rowData) => {}
-    const confirmDeleteItem = (rowData) => {}
+const Table: React.FC<Props> = ({ name, content, columns }) => {
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [selectedContent, SetSelectedContent] = useState(null)
+  const dt = useRef()
+  const editItem = (rowData) => { }
+  const confirmDeleteItem = (rowData) => { }
 
-    const renderHeader = () => {
-        return (
-          <div className={`${classes.table_header} table-header`}>
-            <div>
-              <span className="p-input-icon-left">
-                <i className="pi pi-search" />
-                <InputText
-                  type="search"
-                  onChange={(e) => setGlobalFilter(e.target.value)}
-                  placeholder="Global search"
-                />
-              </span>
-            </div>
-    
-            <div className={classes.button_group}>
-              <Button
-                label="Delete"
-                icon="pi pi-trash"
-                className="p-button-danger p-button-rounded"
-                onClick={() => confirmDeleteItem(selectedContent)}
-              />
-              <Button label="New" icon="pi pi-plus" className="p-button-rounded" />
-            </div>
-          </div>
-        );
-      };
-    const header = renderHeader();
-    const actionBodyTemplate = (rowData) => {
-        return (
-            <div className={classes.actions_field}>
-                <Button icon="pi pi-pencil" className="p-button-rounded p-button-outlined p-mr-2" onClick={() => editItem(rowData)} />
-                <Button icon="pi pi-trash" className="p-button-rounded p-button-outlined" onClick={() => confirmDeleteItem(rowData)} />
-            </div>
-        );
-    }
-    const columnComponents = columns.map((col,index) => {
-        return(
-            <Column
-                key={col.field}
-                field={col.field}
-                header={col.header}
-                filter
-                sortable
-                filterPlaceholder={col.filterPlaceholder}/>
-        )
-    })
+  const renderHeader = () => {
     return (
-        <DataTable 
-            ref={dt}
-            value={content || []}
-            rowHover
-            header={header}
-            selection={selectedContent}
-            globalFilter={globalFilter}
-            onSelectionChange={(e) => SetSelectedContent(e.value)}
-            emptyMessage={`No ${name} found`}
-            >
-            <Column selectionMode="multiple" style={{ width: "3em" }} />   
-            {columnComponents}
-            <Column className={classes.center} header="Actions" body={actionBodyTemplate}></Column>
-        </DataTable>
+      <div className={`${classes.table_header} table-header`}>
+        <div>
+          <span className="p-input-icon-left">
+            <i className="pi pi-search" />
+            <InputText
+              type="search"
+              onChange={(e) => setGlobalFilter(e.target.value)}
+              placeholder="Global search"
+            />
+          </span>
+        </div>
+
+        <div className={classes.button_group}>
+          <Button
+            label="Delete"
+            icon="pi pi-trash"
+            className="p-button-danger p-button-rounded"
+            onClick={() => confirmDeleteItem(selectedContent)}
+          />
+          <Button label="New" icon="pi pi-plus" className="p-button-rounded" />
+        </div>
+      </div>
+    );
+  };
+  const header = renderHeader();
+  const actionBodyTemplate = (rowData) => {
+    return (
+      <div className={classes.actions_field}>
+        <Button icon="pi pi-pencil" className="p-button-rounded p-button-outlined p-mr-2" onClick={() => editItem(rowData)} />
+        <Button icon="pi pi-trash" className="p-button-rounded p-button-outlined" onClick={() => confirmDeleteItem(rowData)} />
+      </div>
+    );
+  }
+  const columnComponents = columns.map((col, index) => {
+    return (
+      <Column
+        key={col.field}
+        field={col.field}
+        header={col.header}
+        filter
+        sortable
+        filterPlaceholder={col.filterPlaceholder} />
     )
+  })
+  return (
+    <div className="datatable-responsive-demo">
+      <div className="card">
+        <DataTable
+          className="p-datatable-responsive-demo"
+          ref={dt}
+          value={content || []}
+          rowHover
+          header={header}
+          selection={selectedContent}
+          globalFilter={globalFilter}
+          onSelectionChange={(e) => SetSelectedContent(e.value)}
+          emptyMessage={`No ${name} found`}
+        >
+          <Column selectionMode="multiple" style={{ width: "3em" }} />
+          {columnComponents}
+          <Column className={classes.center} header="Actions" body={actionBodyTemplate}></Column>
+        </DataTable>
+      </div>
+    </div>
+  )
 }
 export default Table
