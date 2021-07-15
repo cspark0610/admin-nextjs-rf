5fe6b0e11fb15b7339b8ff6270591e710303e138
Anthony Moreno <anthony560@hotmail.com>
2021-07-15 10:40:46 -0400

    change props in map icon

--- src/components/UI/Organism/Map.tsx
+++ src/components/UI/Organism/Map.tsx
@@ -40,9 +40,9 @@ export default function Map( {familyCenter = center, markers = null} ) {
                 // onClick={() => setDetails(marker)}
                 icon={{
                   url: marker.icon,
-                  scaledSize: new window.google.maps.Size(30, 30),
-                  origin: new window.google.maps.Point(0, 0),
-                  anchor: new window.google.maps.Point(15, 15)
+                  // scaledSize: new window.google.maps.Size(30, 30),
+                  // origin: new window.google.maps.Point(0, 0),
+                  // anchor: new window.google.maps.Point(15, 15)
                 }}
               />
             ))
