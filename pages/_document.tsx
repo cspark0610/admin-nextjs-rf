e064722a79b638d35c4e89c838420bd5405accbf
Anthony Moreno <anthony560@hotmail.com>
2021-07-20 16:39:40 -0400

    add google maps from API 🗺

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,10 @@ class MyDocument extends Document {
         <Head>
           <link rel='shortcut icon' href='/favicon.png' />
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+          <script async
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBA6CMSKf1VIijJRH8Q8d9qGk8ZL4APhF0&callback=initMap">
+          </script>
         </Head>
         <body>
           <Main />
