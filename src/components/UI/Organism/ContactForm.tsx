ad354ff6a195e941dd6686652a147cfbd4c9415c
fonscode <fons.code@gmail.com>
2021-08-03 12:46:46 -0400

    ♻ Refactor to contact form with validations and formik

--- src/components/UI/Organism/ContactForm.tsx
+++ src/components/UI/Organism/ContactForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useRef } from 'react'
 //components
 import InputContainer from 'components/UI/Molecules/InputContainer'
-import {Button }from 'primereact/button'
+import { Button } from 'primereact/button'
 import Icon from 'components/UI/Atoms/Icon'
 import { Panel } from 'primereact/panel'
 import { Toast } from 'primereact/toast'
@@ -14,20 +14,20 @@ import classes from "styles/Families/Forms.module.scss"
 //Context
 import { FamilyContext } from 'context/FamilyContext'
 //utils
-import {validateEmail} from 'utils/validations'
+import { validateEmail , validatePhoneNumber, validateURL} from 'utils/validations'
 //services
 import FamiliesService from 'services/Families'
 import { useSession } from 'next-auth/client'
 
 type contactData = {
-    email:string
+    email: string
     zoom: string
-    facebook: string
+    facebookMessenger: string
     whatsapp: string
     line: string
     teams: string
-    googleMeet:string
-    skype:string
+    googleMeet: string
+    skype: string
 }
 export default function ContactForm() {
     const { family, setFamily } = useContext(FamilyContext)
@@ -50,135 +50,175 @@ export default function ContactForm() {
         },
         validate: data => {
             let errors: Partial<contactData> = {}
-
-            if(data.email === ''){
-                errors.email = 'Email is required'
-            }
-            if(validateEmail(data.email)){
+            if (!validateEmail(data.email)) {
                 errors.email = 'Your email is not valid'
             }
+            if(!validatePhoneNumber(data.skype)){
+                errors.skype = 'Your Skype number is not valid'
+            }
+            if(!validatePhoneNumber(data.whatsapp)){
+                errors.whatsapp = "Your Whatsapp number is not valid"
+            }
+            if(!validateURL(data.facebookMessenger)){
+                errors.facebookMessenger = "Your Facebook url its not valid"
+            }
+            if(!validatePhoneNumber(data.line)){
+                errors.line = "Your Line number is not valid"
+            }
+            if(!validateEmail(data.email)){
+                errors.email = "Your email address is not valid"
+            }
+            if(!validateEmail(data.teams)){
+                errors.teams = "Your Teams email is not valid"
+            }
+            if(!validateURL(data.zoom)){
+                errors.zoom = "Your Zoom url is not valid"
+            }
+            if(!validateURL(data.googleMeet)){
+                errors.googleMeet = "Your Google Meet url is not valid"
+            }
+            return errors
         },
-        onSubmit: data=> {
+        onSubmit: data => {
             handleSubmit(data)
             formik.resetForm()
         }
     })
     const showSuccess = () => {
-        toast.current.show({severity:'success', summary: 'Success Message', detail:'Contact accounts successfully updateds', life: 3000});
+        toast.current.show({ severity: 'success', summary: 'Success Message', detail: 'Contact accounts successfully updateds', life: 3000 });
     }
     const showError = () => {
-        toast.current.show({severity:'error', summary: 'Error Message', detail:'An error has ocurred', life: 3000});
+        toast.current.show({ severity: 'error', summary: 'Error Message', detail: 'An error has ocurred', life: 3000 });
     }
 
     const handleSubmit = (data) => {
         setLoading(true)
         FamiliesService.updatefamily(session?.token, family._id, data)
-        .then(()=> {
-            setLoading(false)
-            showSuccess()
-            setFamily({...family, data})
-        })
-        .catch(err => {
-            console.log(err)
-            setLoading(false)
-            showError()
-        })
+            .then(() => {
+                setLoading(false)
+                showSuccess()
+                setFamily({ ...family, data })
+            })
+            .catch(err => {
+                console.log(err)
+                setLoading(false)
+                showError()
+            })
 
     }
+    const isFormFieldValid = (name) => !!(formik.touched[name] && formik.errors[name]);
+    const getFormErrorMessage = (name) => {
+        return isFormFieldValid(name) && <small className="p-error">{formik.errors[name]}</small>;
+    };
 
     return (
         <Panel header="The Best Way For The Student To Contact The Family" toggleable style={{ marginTop: '3rem' }}>
             <form onSubmit={formik.handleSubmit}>
-            <div className={classes.form_container_multiple}>
-                <InputContainer label="Skype">
-                    <span className="p-input-icon-right">
-                        <Icon svg="skype" classes={styles.small} />
-                        <InputText
-                            id="skype"
-                            placeholder="Skype account"
-                            value={formik.values.skype}
-                            onChange={formik.handleChange}
-                        />
-                    </span>
-                </InputContainer>
-                <InputContainer label="Whatsapp">
-                    <span className="p-input-icon-right">
-                        <Icon svg="whatsapp" classes={styles.small} />
-                        <InputText
-                            id="whatsapp"
-                            placeholder="Whatsapp account"
-                            value={formik.values.whatsapp}
-                            type='email'
-                            required
-                            onChange={formik.values.whatsapp}
-                        />
-                    </span>
-                </InputContainer>
-                <InputContainer label="Facebook Messenger">
-                    <span className="p-input-icon-right">
-                        <Icon svg="messenger" classes={styles.small} />
-                        <InputText
-                            id="facebookMessenger"
-                            placeholder="Facebook account"
-                            value={formik.values.facebookMessenger}
-                            onChange={formik.handleChange} />
-                    </span>
-                </InputContainer>
-                <InputContainer label="Line">
-                    <span className="p-input-icon-right">
-                        <Icon svg="line" classes={styles.small} />
-                        <InputText
-                            id='line'
-                            placeholder="Line account"
-                            value={formik.values.line}
-                            onChange={formik.values.line} />
-                    </span>
-                </InputContainer>
-                <InputContainer label="Email">
-                    <span className="p-input-icon-right">
-                        <Icon svg="gmail" classes={styles.small} />
-                        <InputText
-                            id="email"
-                            placeholder="Email account"
-                            value={formik.values.email}
-                            onChange={formik.handleChange} />
-                    </span>
-                </InputContainer>
-                <InputContainer label="Teams">
-                    <span className="p-input-icon-right">
-                        <Icon svg="teams" classes={styles.small} />
-                        <InputText
-                            id="teams"
-                            placeholder="Teams account"
-                            value={formik.values.teams}
-                            onChange={formik.handleChange} />
-                    </span>
-                </InputContainer>
-                <InputContainer label="Zoom">
-                    <span className="p-input-icon-right">
-                        <Icon svg="zoom" classes={styles.small} />
-                        <InputText
-                            id="zoom"
-                            placeholder="Zoom account"
-                            value={formik.values.zoom}
-                            onChange={formik.handleChange} />
-                    </span>
-                </InputContainer>
-                <InputContainer label="Google meet">
-                    <span className="p-input-icon-right">
-                        <Icon svg="meet" classes={styles.small} />
-                        <InputText
-                            id="googleMeet"
-                            placeholder="Google meet account"
-                            value={formik.values.googleMeet}
-                            onChange={ formik.handleChange} />
-                    </span>
-                </InputContainer>
-            </div>
-            <div style={{width:'100%', display: 'flex', justifyContent: 'flex-end'}}>
-                <Button label="Save" type='submit' loading={loading}  icon="pi pi-save" className="p-button-rounded"/>
-            </div>
-            <Toast ref={toast} />
+                <div className={classes.form_container_multiple}>
+                    <InputContainer label="Skype" 
+                        labelClass={classNames({ 'p-error': isFormFieldValid('skype') })}>
+                        <span className="p-input-icon-right">
+                            <Icon svg="skype" classes={styles.small} />
+                            <InputText
+                                id="skype"
+                                placeholder="Skype account"
+                                value={formik.values.skype}
+                                onChange={formik.handleChange}
+                                className={classNames({ 'p-invalid': isFormFieldValid('skype') })}
+                            />
+                        </span>
+                        {getFormErrorMessage('skype')}
+                    </InputContainer>
+                    <InputContainer label="Whatsapp" labelClass={classNames({ 'p-error': isFormFieldValid('whatsapp') })}>
+                        <span className="p-input-icon-right">
+                            <Icon svg="whatsapp" classes={styles.small} />
+                            <InputText
+                                id="whatsapp"
+                                placeholder="Whatsapp account"
+                                value={formik.values.whatsapp}
+                                className={classNames({ 'p-invalid': isFormFieldValid('whatsapp') })}
+                                onChange={formik.handleChange}
+                            />
+                        </span>
+                        {getFormErrorMessage('whatsapp')}
+                    </InputContainer>
+                    <InputContainer label="Facebook Messenger" labelClass={classNames({ 'p-error': isFormFieldValid('facebookMessenger') })}>
+                        <span className="p-input-icon-right">
+                            <Icon svg="messenger" classes={styles.small} />
+                            <InputText
+                                id="facebookMessenger"
+                                placeholder="Facebook account"
+                                value={formik.values.facebookMessenger}
+                                className={classNames({ 'p-invalid': isFormFieldValid('facebookMessenger') })}
+                                onChange={formik.handleChange} />
+                        </span>
+                        {getFormErrorMessage('facebookMessenger')}
+                    </InputContainer>
+                    <InputContainer label="Line" labelClass={classNames({ 'p-error': isFormFieldValid('line') })}>
+                        <span className="p-input-icon-right">
+                            <Icon svg="line" classes={styles.small} />
+                            <InputText
+                                id='line'
+                                placeholder="Line account"
+                                className={classNames({ 'p-invalid': isFormFieldValid('line') })}
+                                value={formik.values.line}
+                                onChange={formik.handleChange} />
+                        </span>
+                        {getFormErrorMessage('line')}
+                    </InputContainer>
+                    <InputContainer label="Email" labelClass={classNames({ 'p-error': isFormFieldValid('email') })}>
+                        <span className="p-input-icon-right">
+                            <Icon svg="gmail" classes={styles.small} />
+                            <InputText
+                                id="email"
+                                className={classNames({ 'p-invalid': isFormFieldValid('email') })}
+                                placeholder="Email account"
+                                value={formik.values.email}
+                                onChange={formik.handleChange} />
+                        </span>
+                        {getFormErrorMessage('email')}
+                    </InputContainer>
+                    <InputContainer label="Teams" labelClass={classNames({ 'p-error': isFormFieldValid('teams') })}>
+                        <span className="p-input-icon-right">
+                            <Icon svg="teams" classes={styles.small} />
+                            <InputText
+                                id="teams"
+                                placeholder="Teams account"
+                                className={classNames({ 'p-invalid': isFormFieldValid('teams') })}
+                                value={formik.values.teams}
+                                onChange={formik.handleChange} />
+                        </span>
+                        {getFormErrorMessage('teams')}
+                    </InputContainer>
+                    <InputContainer label="Zoom" labelClass={classNames({ 'p-error': isFormFieldValid('zoom') })}>
+                        <span className="p-input-icon-right">
+                            <Icon svg="zoom" classes={styles.small} />
+                            <InputText
+                                id="zoom"
+                                placeholder="Zoom account"
+                                className={classNames({ 'p-invalid': isFormFieldValid('zoom') })}
+                                value={formik.values.zoom}
+                                onChange={formik.handleChange} />
+                        </span>
+                        {getFormErrorMessage('zoom')}
+                    </InputContainer>
+                    <InputContainer label="Google meet" labelClass={classNames({ 'p-error': isFormFieldValid('googleMeet') })}>
+                        <span className="p-input-icon-right">
+                            <Icon svg="meet" classes={styles.small} />
+                            <InputText
+                                id="googleMeet"
+                                placeholder="Google meet account"
+                                className={classNames({ 'p-invalid': isFormFieldValid('googleMeet') })}
+                                value={formik.values.googleMeet}
+                                onChange={formik.handleChange} />
+                        </span>
+                        {getFormErrorMessage('googleMeet')}
+                    </InputContainer>
+                </div>
+                <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+                    <Button label="Save" type='submit' loading={loading} icon="pi pi-save" className="p-button-rounded" />
+                </div>
+                <Toast ref={toast} />
 
             </form>
         </Panel>
