fcc8be71b11a12ab9333c893a5bbeeb2a1ce4d4a
fonscode <fons.code@gmail.com>
2021-08-06 15:06:13 -0400

    ✨Feat: viewer in family pictures

--- src/components/UI/Organism/Gallery.tsx
+++ src/components/UI/Organism/Gallery.tsx
@@ -2,19 +2,30 @@ import React,{useState} from 'react'
 import { Galleria } from 'primereact/galleria';
 import dynamic from 'next/dynamic'
 
-const itemTemplate = ({src, alt}) => {
-    return <img src={src} alt={alt} style={{ maxWidth: '100%', aspectRatio:'2/1' }} />
-}
+
 const thumbnailTemplate = ({src, alt}) => {
     return <img src={src} alt={alt}  style={{ maxWidth: '100px', width:'100%', marginRight:'3vw', aspectRatio:'1/1' }}/>
 }
 export default function Gallery({images}) {
     const [showViewer, setShowViewer] = useState(false)
     const Viewer = dynamic(()=> import('react-viewer'), {ssr: false})
+    const [selectedItem, setSelectedItem] = useState(0)
+    
+    const itemTemplate = ({src, alt, id}) => {
+        setSelectedItem(id)
+        return <img src={src} alt={alt}  onClick={()=>{setShowViewer(true)}} style={{ maxWidth: '100%', aspectRatio:'2/1', cursor:'pointer' }} />
+        }
     return (
         <>
-            <Galleria value={images} item={itemTemplate} numVisible={5} thumbnail={thumbnailTemplate} style={{ maxWidth: '640px' }}/>
-            <Viewer visible={showViewer} images={images}/>
+            <div>
+                <Galleria 
+                    value={images} 
+                    item={itemTemplate} 
+                    numVisible={5} 
+                    thumbnail={thumbnailTemplate} 
+                    style={{ maxWidth: '640px' }}/>
+            </div>
+            <Viewer activeIndex={selectedItem} onClose={()=> {setShowViewer(false)}} visible={showViewer} images={images}/>
         </>
     )
 }
