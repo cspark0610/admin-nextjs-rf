4eb5948b53ee6b6a7b3298ac4178c63ae0b4916c
fonscode <fons.code@gmail.com>
2021-08-09 09:37:45 -0400

    Fix galleria styles in family tab

---
+++ src/components/Families/modals/FamilyPicturesForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+//components
+import InputContainer from 'components/UI/Molecules/InputContainer'
+import FileUploader from 'components/UI/Atoms/FileUploader'
+import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
+
+const FamilyPicturesForm = () => {
+    return(
+        <form>
+            form
+        </form>
+    )
+}
+export default FamilyPicturesForm
--- src/components/UI/Organism/Gallery.tsx
+++ src/components/UI/Organism/Gallery.tsx
@@ -1,31 +1,61 @@
-import React,{useState} from 'react'
+import React, { useState, useRef } from 'react'
 import { Galleria } from 'primereact/galleria';
 import dynamic from 'next/dynamic'
+import { Menu } from 'primereact/menu';
+import { Tooltip } from 'primereact/tooltip';
+import Modal from 'components/UI/Molecules/Modal'
+//styles
+import classes from 'styles/UI/Organism/Gallery.module.scss'
 
 
-const thumbnailTemplate = ({src, alt}) => {
-    return <img src={src} alt={alt}  style={{ maxWidth: '100px', width:'100%', marginRight:'3vw', aspectRatio:'1/1' }}/>
+const thumbnailTemplate = ({ src, alt }) => {
+    return <>
+        <Tooltip target={`#thumbnail-${alt}`}>{alt}</Tooltip>
+        <img id={`thumbnail-${alt}`} className={`${classes.thumbnail}`} src={src} alt={alt} style={{ maxWidth: '100px', width: '100%', marginRight: '3vw', aspectRatio: '1/1' }} />
+    </>
 }
-export default function Gallery({images}) {
+
+
+export default function Gallery({ images }) {
+    const menu = useRef(null)
     const [showViewer, setShowViewer] = useState(false)
-    const Viewer = dynamic(()=> import('react-viewer'), {ssr: false})
+    const Viewer = dynamic(() => import('react-viewer'), { ssr: false })
     const [selectedItem, setSelectedItem] = useState(0)
-    
-    const itemTemplate = ({src, alt, id}) => {
+    const [showCreateModal, setShowCreateModal] = useState(false)
+
+    const itemTemplate = ({ src, alt, id }) => {
         setSelectedItem(id)
-        return <img src={src} alt={alt}  onClick={()=>{setShowViewer(true)}} style={{ maxWidth: '100%', aspectRatio:'2/1', cursor:'pointer' }} />
-        }
+        return <img className={classes.image} src={src} alt={alt} onClick={() => { setShowViewer(true) }} style={{ maxWidth: '100%', aspectRatio: '2/1', cursor: 'pointer' }} />
+    }
+    let menuItems = [
+    { label: 'New', icon: 'pi pi-fw pi-plus',  command: ()=>{setShowCreateModal(true)}},
+    { label: 'Edit', icon: 'pi pi-pencil' },
+    { label: 'Delete', icon: 'pi pi-fw pi-trash' }];
     return (
         <>
-            <div>
-                <Galleria 
-                    value={images} 
-                    item={itemTemplate} 
-                    numVisible={5} 
-                    thumbnail={thumbnailTemplate} 
-                    style={{ maxWidth: '640px' }}/>
+            <div className={classes.container}>
+                <Galleria
+                    className={classes.gallery}
+                    value={images}
+                    item={itemTemplate}
+                    numVisible={5}
+                    thumbnail={thumbnailTemplate}
+                    style={{ maxWidth: '640px' }} />
+                <Tooltip target=".menu" position="left">Options</Tooltip><button
+                    className={`${classes.menu_button} menu`}
+                    onClick={(event) => menu.current.toggle(event)}
+                >
+                    <i className="pi pi-ellipsis-v"></i>
+                </button>
+                <Menu
+                    model={menuItems}
+                    popup ref={menu}
+                />
             </div>
-            <Viewer activeIndex={selectedItem} onClose={()=> {setShowViewer(false)}} visible={showViewer} images={images}/>
+            <Viewer activeIndex={selectedItem} onClose={() => { setShowViewer(false) }} visible={showViewer} images={images} />
+            <Modal title="Add new family photos" visible={showCreateModal} setVisible={setShowCreateModal} icon="family">
+                <h1>hi</h1>
+            </Modal>
         </>
     )
 }
