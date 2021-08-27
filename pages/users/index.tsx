59e52088e76605320f847784066be90e62a80b28
josejmv <josejmvasquez@gmail.com>
2021-08-27 12:30:10 -0400

    verify deploy 4

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 // components
-import Layout from 'components/Layout'
-import Datatable from 'components/Users/Datatable'
+// import Layout from 'components/Layout'
+// import Datatable from 'components/Users/Datatable'
 
 export default function UsersPage() {
-    return (
-        <Layout>
-            <h1>Users</h1>
-            <Datatable />
-        </Layout>
-    )
+  return (
+    <></>
+    // <Layout>
+    //     <h1>Users</h1>
+    //     <Datatable />
+    // </Layout>
+  )
 }
