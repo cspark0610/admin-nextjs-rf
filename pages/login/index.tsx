3baa30efbf6ed4b20b911062aa0f661e99bf1d07
josejmv <josejmvasquez@gmail.com>
2021-08-27 13:15:28 -0400

    verify deploy 20

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -31,7 +31,7 @@ export default function FamilyPage() {
           </Button>
         </>
       )}
-      {/* {showLoginForm && <LoginForm />} */}
+      {showLoginForm && <LoginForm />}
     </div>
   )
 }
