150ef14c7dc60990354c472e83eed059147888a6
josejmv <josejmvasquez@gmail.com>
2021-08-27 12:58:30 -0400

    verify deploy 14

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,26 +1,26 @@
-// import { useState, useEffect } from 'react'
-// import { Button } from 'primereact/button'
+import { useState, useEffect } from 'react'
+import { Button } from 'primereact/button'
 import classes from 'styles/Home/Home.module.scss'
-// import LoginForm from 'components/Login'
-// import { useSession } from 'next-auth/client'
-// import { useRouter } from 'next/router'
+import LoginForm from 'components/Login'
+import { useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
 
 export default function FamilyPage() {
-  // const [showLoginForm, setShowLoginForm] = useState(false)
-  // const [session, loading] = useSession()
-  // const { push } = useRouter()
+  const [showLoginForm, setShowLoginForm] = useState(false)
+  const [session, loading] = useSession()
+  const { push } = useRouter()
 
-  // useEffect(() => {
-  //   if (!loading && !session) {
-  //     push('/login')
-  //   } else if (!loading && session) {
-  //     push('/')
-  //   }
-  // }, [session, loading])
+  useEffect(() => {
+    if (!loading && !session) {
+      push('/login')
+    } else if (!loading && session) {
+      push('/')
+    }
+  }, [session, loading])
 
   return (
     <div className={classes.home}>
-      {/* {!showLoginForm && (
+      {!showLoginForm && (
         <>
           <img src='/assets/logo-redleaf.svg' alt='logo redleaf' />
           <Button
@@ -31,7 +31,7 @@ export default function FamilyPage() {
           </Button>
         </>
       )}
-      {showLoginForm && <LoginForm />} */}
+      {showLoginForm && <LoginForm />}
     </div>
   )
 }
