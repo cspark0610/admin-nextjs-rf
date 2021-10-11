66e674879be92558aab8d86c07890f6bb0211abc
Ubaldo <sarcosas.dev@gmail.com>
2021-10-11 17:17:08 -0400

    added pagination for families and users

--- src/components/Users/Datatable/index.tsx
+++ src/components/Users/Datatable/index.tsx
@@ -260,6 +260,9 @@ const Datatable = () => {
             sortOrder={1}
             defaultSortOrder={1}
             onSelectionChange={(e) => setSelectedUsers(e.value)}
+            paginator={true}
+            rows={20}
+            rowsPerPageOptions={[10, 20, 50]}
           >
             <Column selectionMode='multiple' style={{ width: '3em' }} />
             {selectedColumns.map((column) => {
