17c49441b3739e9b9cb198f2e0b7084f29c2876a
josejmv <josejmvasquez@gmail.com>
2021-11-03 00:43:21 -0400

    ready to deploy at aws

--- src/services/Families.ts
+++ src/services/Families.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const msFamily = 'ms-fands'
+const msFamily = 'ms-fands/api/v1'
 export default class FamiliesService {
   static createFamily(token, data) {
     return axios({
