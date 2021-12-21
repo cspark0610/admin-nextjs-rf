5b3202d36673c0f9d8ea25178c0fea35f4082fdf
josejmv <josejmvasquez@gmail.com>
2021-12-21 09:53:34 -0400

    [feature] import families page

--- pages/families/index.tsx
+++ pages/families/index.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+// main tools
+import Link from 'next/link'
+
+// prime components
+import { Button } from 'primereact/button'
+
 // components
 import Layout from 'components/Layout'
 import Datatable from 'components/Families/datatable'
 
+// styles
+import classes from 'styles/Families/import.module.scss'
+
 export default function FamilyPage() {
   return (
     <Layout>
-      <h1>Families</h1>
+      <div className={classes.pageTitle}>
+        <h1>Families</h1>
+        <Link href='families/import'>
+          <a className='p-button-link export-button'>
+            <Button
+              label='Go to import'
+              icon='pi pi-external-link'
+              className='p-button-link export-button'
+            />
+          </a>
+        </Link>
+      </div>
       <Datatable />
     </Layout>
   )
