9e4ccfa361277653fef937d8186a8bd31827365c
Ubaldo <sarcosas.dev@gmail.com>
2022-02-15 16:31:43 -0400

    studentsRoom now get an nimber when press save in homeDetails form

--- src/components/Families/forms/RegisterFamily/Home.tsx
+++ src/components/Families/forms/RegisterFamily/Home.tsx
@@ -1,65 +1,74 @@
-import { useContext, useEffect, useState } from 'react'
-import { RegisterFamilyContext } from 'context/RegisterFamilyContext'
-import { useSession } from 'next-auth/client'
-import { Button } from 'primereact/button'
-import GenericsService from 'services/Generics'
-import InputContainer from 'components/UI/Molecules/InputContainer'
-import { Dropdown } from 'primereact/dropdown'
-import { MultiSelect } from 'primereact/multiselect'
-import { AvailabilityPicker } from 'components/UI/Atoms/AvailabilityPicker'
-import CreatableSelect from 'react-select/creatable'
+import { useContext, useEffect, useState } from "react";
+import { RegisterFamilyContext } from "context/RegisterFamilyContext";
+import { useSession } from "next-auth/client";
+import { Button } from "primereact/button";
+import GenericsService from "services/Generics";
+import InputContainer from "components/UI/Molecules/InputContainer";
+import { Dropdown } from "primereact/dropdown";
+import { MultiSelect } from "primereact/multiselect";
+import { AvailabilityPicker } from "components/UI/Atoms/AvailabilityPicker";
+import CreatableSelect from "react-select/creatable";
 
 const STUDENT_ROOM_INITIAL_VALUES = {
   aditionalFeatures: [],
   availability: [],
-  bathType: '',
-  bathroomLocation: '',
-  bedType: '',
-  floor: '',
-  type: '',
-}
+  bathType: "",
+  bathroomLocation: "",
+  bedType: "",
+  floor: "",
+  type: "",
+  roomNumber: 0,
+};
 
 const Home = () => {
-  const [session] = useSession()
+  const [session] = useSession();
 
   const {
     family: { home },
     setHome,
     setStudentRooms,
-  } = useContext(RegisterFamilyContext)
+  } = useContext(RegisterFamilyContext);
 
-  const [count, setCount] = useState(0)
-  const [homeTypes, setHomeTypes] = useState([])
-  const [roomTypes, setRoomTypes] = useState([])
-  const [services, setServices] = useState([])
-  const [additionalRoomFeatures, setAdditionalRoomFeatures] = useState([])
-  const [nearbyServices, setNearbyServices] = useState([])
+  const [count, setCount] = useState(0);
+  const [homeTypes, setHomeTypes] = useState([]);
+  const [roomTypes, setRoomTypes] = useState([]);
+  const [services, setServices] = useState([]);
+  const [additionalRoomFeatures, setAdditionalRoomFeatures] = useState([]);
+  const [nearbyServices, setNearbyServices] = useState([]);
 
-  const handleChange = (field, value) => setHome({ ...home, [field]: value })
+  const handleChange = (field, value) => setHome({ ...home, [field]: value });
 
   const handleIncrement = () => {
-    setCount(count + 1)
-    let auxStudentRooms = [...home.studentRooms]
-    auxStudentRooms.push(STUDENT_ROOM_INITIAL_VALUES)
-    setStudentRooms(auxStudentRooms)
-  }
+    setCount(count + 1);
+    let auxStudentRooms = [...home.studentRooms];
+    auxStudentRooms.push(STUDENT_ROOM_INITIAL_VALUES);
+    setStudentRooms(auxStudentRooms);
+  };
 
   const handleDecrement = () => {
     if (count - 1 >= 0) {
-      setCount(count - 1)
-      let auxStudentRooms = [...home.studentRooms]
-      setStudentRooms(auxStudentRooms.slice(0, count - 1))
+      setCount(count - 1);
+      let auxStudentRooms = [...home.studentRooms];
+      setStudentRooms(auxStudentRooms.slice(0, count - 1));
     }
-  }
+  };
 
   const handleRoomChange = (index, field, value) => {
-    let auxStudentRooms = [...home.studentRooms]
-    auxStudentRooms[index] = { ...auxStudentRooms[index], [field]: value }
-    setStudentRooms(auxStudentRooms)
-  }
+    let auxStudentRooms = [...home.studentRooms];
+    if (auxStudentRooms[index]?.roomNumber) {
+      auxStudentRooms[index] = { ...auxStudentRooms[index], [field]: value };
+    } else {
+      auxStudentRooms[index] = {
+        ...auxStudentRooms[index],
+        roomNumber: index + 1,
+        [field]: value,
+      };
+    }
+    setStudentRooms(auxStudentRooms);
+  };
 
   useEffect(() => {
-    ;(async () => {
+    (async () => {
       const {
         homeTypes,
         roomTypes,
@@ -67,15 +76,15 @@ const Home = () => {
         additionalRoomFeatures,
         nearbyServices,
       } = await GenericsService.getAll(session?.token, [
-        'homeTypes',
-        'roomTypes',
-        'nearbyServices',
-        'additionalRoomFeatures',
-        'services',
-      ])
+        "homeTypes",
+        "roomTypes",
+        "nearbyServices",
+        "additionalRoomFeatures",
+        "services",
+      ]);
 
-      setHomeTypes(homeTypes)
-      setRoomTypes(roomTypes)
+      setHomeTypes(homeTypes);
+      setRoomTypes(roomTypes);
 
       setAdditionalRoomFeatures(
         additionalRoomFeatures.map((service) => ({
@@ -85,7 +94,7 @@ const Home = () => {
           label: service.name,
           isFreeComment: false,
         }))
-      )
+      );
 
       setServices(
         services.map((service) => ({
@@ -95,7 +104,7 @@ const Home = () => {
           label: service.name,
           isFreeComment: false,
         }))
-      )
+      );
 
       setNearbyServices(
         nearbyServices.map((nearbyService) => ({
@@ -105,163 +114,163 @@ const Home = () => {
           label: nearbyService.name,
           isFreeComment: false,
         }))
-      )
-    })()
-  }, [session])
+      );
+    })();
+  }, [session]);
 
   return (
     <>
-      <div className='two-columns'>
-        <InputContainer label='House Type'>
+      <div className="two-columns">
+        <InputContainer label="House Type">
           <Dropdown
             options={homeTypes}
             value={home.homeType}
-            optionLabel='name'
-            name='homeType'
-            onChange={({ value }) => handleChange('homeType', value)}
-            placeholder='Select type'
+            optionLabel="name"
+            name="homeType"
+            onChange={({ value }) => handleChange("homeType", value)}
+            placeholder="Select type"
           />
         </InputContainer>
-        <InputContainer label='Inside'>
+        <InputContainer label="Inside">
           <MultiSelect
-            name='houseRooms'
+            name="houseRooms"
             value={home.houseRooms}
             options={roomTypes}
-            onChange={({ value }) => handleChange('houseRooms', value)}
-            optionLabel='name'
-            selectedItemTemplate={(item) => (item ? `${item?.name}, ` : '')}
-            placeholder='Select rooms'
+            onChange={({ value }) => handleChange("houseRooms", value)}
+            optionLabel="name"
+            selectedItemTemplate={(item) => (item ? `${item?.name}, ` : "")}
+            placeholder="Select rooms"
           />
         </InputContainer>
-        <InputContainer label='Household Amenities'>
+        <InputContainer label="Household Amenities">
           <MultiSelect
-            name='services'
+            name="services"
             value={home.services}
             options={services}
-            onChange={({ value }) => handleChange('services', value)}
-            optionLabel='value.name'
-            selectedItemTemplate={(item) => (item ? `${item?.name}, ` : '')}
-            placeholder='Select Services'
+            onChange={({ value }) => handleChange("services", value)}
+            optionLabel="value.name"
+            selectedItemTemplate={(item) => (item ? `${item?.name}, ` : "")}
+            placeholder="Select Services"
           />
         </InputContainer>
-        <InputContainer label='Nearby Services'>
+        <InputContainer label="Nearby Services">
           <MultiSelect
-            name='nearbyServices'
+            name="nearbyServices"
             value={home.nearbyServices}
             options={nearbyServices}
-            onChange={({ value }) => handleChange('nearbyServices', value)}
-            optionLabel='value.name'
-            selectedItemTemplate={(item) => (item ? `${item?.name}, ` : '')}
-            placeholder='Select Nearby Services'
+            onChange={({ value }) => handleChange("nearbyServices", value)}
+            optionLabel="value.name"
+            selectedItemTemplate={(item) => (item ? `${item?.name}, ` : "")}
+            placeholder="Select Nearby Services"
           />
         </InputContainer>
       </div>
-      <div style={{ margin: '1rem 0' }}>
+      <div style={{ margin: "1rem 0" }}>
         <p>How many rooms you have?</p>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <Button
-            type='button'
-            icon='pi pi-minus-circle'
-            className='p-button-rounded p-button-info p-button-text'
+            type="button"
+            icon="pi pi-minus-circle"
+            className="p-button-rounded p-button-info p-button-text"
             onClick={handleDecrement}
           />
-          <span style={{ margin: 'auto 0.5rem' }}>{count}</span>
+          <span style={{ margin: "auto 0.5rem" }}>{count}</span>
           <Button
-            type='button'
-            icon='pi pi-plus-circle'
-            className='p-button-rounded p-button-info p-button-text'
+            type="button"
+            icon="pi pi-plus-circle"
+            className="p-button-rounded p-button-info p-button-text"
             onClick={handleIncrement}
           />
         </div>
       </div>
       {home.studentRooms.map((room, index) => (
         <>
-          <div className='two-columns'>
-            <InputContainer label='Type'>
+          <div className="two-columns">
+            <InputContainer label="Type">
               <Dropdown
-                options={['Private', 'Shared']}
+                options={["Private", "Shared"]}
                 value={room.type}
-                name='homeType'
-                onChange={({ value }) => handleRoomChange(index, 'type', value)}
-                placeholder='Select type'
+                name="homeType"
+                onChange={({ value }) => handleRoomChange(index, "type", value)}
+                placeholder="Select type"
               />
             </InputContainer>
-            <InputContainer label='Bath Type'>
+            <InputContainer label="Bath Type">
               <Dropdown
-                options={['Private', 'Shared']}
+                options={["Private", "Shared"]}
                 value={room.bathType}
-                name='bathType'
+                name="bathType"
                 onChange={({ value }) =>
-                  handleRoomChange(index, 'bathType', value)
+                  handleRoomChange(index, "bathType", value)
                 }
-                placeholder='Select Bath type'
+                placeholder="Select Bath type"
               />
             </InputContainer>
-            <InputContainer label='Bed Type'>
+            <InputContainer label="Bed Type">
               <Dropdown
                 options={[
-                  'Single',
-                  'Double/Full',
-                  'Queen',
-                  'King',
-                  'Twin/Single',
+                  "Single",
+                  "Double/Full",
+                  "Queen",
+                  "King",
+                  "Twin/Single",
                 ]}
                 value={room.bedType}
-                name='bedType'
+                name="bedType"
                 onChange={({ value }) =>
-                  handleRoomChange(index, 'bedType', value)
+                  handleRoomChange(index, "bedType", value)
                 }
-                placeholder='Select Bed type'
+                placeholder="Select Bed type"
               />
             </InputContainer>
-            <InputContainer label='Floor'>
+            <InputContainer label="Floor">
               <Dropdown
-                options={['Upper Level', 'Main Level', 'Lower Level']}
+                options={["Upper Level", "Main Level", "Lower Level"]}
                 value={room.floor}
-                name='floor'
+                name="floor"
                 onChange={({ value }) =>
-                  handleRoomChange(index, 'floor', value)
+                  handleRoomChange(index, "floor", value)
                 }
-                placeholder='Select Floor'
+                placeholder="Select Floor"
               />
             </InputContainer>
-            <InputContainer label='Bathroom Location'>
+            <InputContainer label="Bathroom Location">
               <Dropdown
-                options={['In the Room', 'Outside of the Room']}
+                options={["In the Room", "Outside of the Room"]}
                 value={room.bathroomLocation}
-                name='bathroomLocation'
+                name="bathroomLocation"
                 onChange={({ value }) =>
-                  handleRoomChange(index, 'bathroomLocation', value)
+                  handleRoomChange(index, "bathroomLocation", value)
                 }
-                placeholder='Select Bathroom location'
+                placeholder="Select Bathroom location"
               />
             </InputContainer>
-            <InputContainer label='Aditional features'>
+            <InputContainer label="Aditional features">
               <MultiSelect
-                name='aditionalFeatures'
+                name="aditionalFeatures"
                 value={room.aditionalFeatures}
                 options={additionalRoomFeatures}
                 onChange={({ value }) =>
-                  handleRoomChange(index, 'aditionalFeatures', value)
+                  handleRoomChange(index, "aditionalFeatures", value)
                 }
-                optionLabel='value.name'
-                selectedItemTemplate={(item) => (item ? `${item?.name}, ` : '')}
-                placeholder='Select Aditional features'
+                optionLabel="value.name"
+                selectedItemTemplate={(item) => (item ? `${item?.name}, ` : "")}
+                placeholder="Select Aditional features"
               />
             </InputContainer>
           </div>
-          <InputContainer label='Availabiliy'>
+          <InputContainer label="Availabiliy">
             <AvailabilityPicker
               dates={room.availability}
               setDates={({ target: { value } }) =>
-                handleRoomChange(index, 'availability', value)
+                handleRoomChange(index, "availability", value)
               }
             />
           </InputContainer>
         </>
       ))}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Families/modals/BedroomModal.tsx
+++ src/components/Families/modals/BedroomModal.tsx
@@ -1,98 +1,125 @@
-import { FC, useState, useEffect } from 'react'
-import InputContainer from 'components/UI/Molecules/InputContainer'
-import { Button } from 'primereact/button'
-import { useFormik } from 'formik'
-import { classNames } from 'primereact/utils'
-import { AvailabilityPicker } from 'components/UI/Atoms/AvailabilityPicker'
+import { FC, useState, useEffect } from "react";
+import InputContainer from "components/UI/Molecules/InputContainer";
+import { Button } from "primereact/button";
+import { useFormik } from "formik";
+import { classNames } from "primereact/utils";
+import { AvailabilityPicker } from "components/UI/Atoms/AvailabilityPicker";
 //utils
-import GenericsService from 'services/Generics'
-import { useSession } from 'next-auth/client'
-import { Dropdown } from 'primereact/dropdown'
-import { MultiSelect } from 'primereact/multiselect'
-import Gallery from 'components/UI/Organism/Gallery'
+import GenericsService from "services/Generics";
+import { useSession } from "next-auth/client";
+import { Dropdown } from "primereact/dropdown";
+import { MultiSelect } from "primereact/multiselect";
+import Gallery from "components/UI/Organism/Gallery";
 
-type bedroomData = any
+type bedroomData = any;
 interface Props {
-  data?: any
-  bedroomPictures: any
-  onSubmit: (e: any) => void
-  setShowPicturesModal: (e: any) => void
+  data?: any;
+  bedroomPictures: any;
+  onSubmit: (e: any) => void;
+  setShowPicturesModal: (e: any) => void;
+  studentRooms: Array<any>;
 }
 
-const Types = ['Private', 'Shared']
-const BathroomLocationTypes = ['In the Room', 'Outside of the Room']
-const BathTypes = ['Private', 'Shared']
-const BedTypes = ['Single', 'Double/Full', 'Queen', 'King', 'Twin/Single']
-const FloorTypes = ['Upper Level', 'Main Level', 'Lower Level']
+const Types = ["Private", "Shared"];
+const BathroomLocationTypes = ["In the Room", "Outside of the Room"];
+const BathTypes = ["Private", "Shared"];
+const BedTypes = ["Single", "Double/Full", "Queen", "King", "Twin/Single"];
+const FloorTypes = ["Upper Level", "Main Level", "Lower Level"];
 
 const BedroomModal: FC<Props> = ({
   data,
   bedroomPictures,
   onSubmit,
   setShowPicturesModal,
+  studentRooms,
 }) => {
-  const [aditional, setAditional] = useState([])
-  const [session] = useSession()
+  const [aditional, setAditional] = useState([]);
+  const [session] = useSession();
+  const [thisRoomNumber, setthisRoomNumber] = useState(studentRooms.length + 1);
 
   useEffect(() => {
-    ;(async () => {
+    if (!!data?.roomNumber) {
+      setthisRoomNumber(data?.roomNumber);
+    } else {
+      let tosum = 0;
+      studentRooms.forEach((room) => {
+        console.log(
+          "aqui la room ",
+          room.roomNumber,
+          thisRoomNumber,
+          " el res "
+        );
+        if (Number(room.roomNumber) === thisRoomNumber) {
+          console.log("sumando esa weputa");
+          tosum++;
+        }
+      });
+      let newval = tosum + thisRoomNumber;
+      setthisRoomNumber(newval);
+      formik.values.roomNumber = newval;
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
       const { additionalRoomFeatures } = await GenericsService.getAll(
         session?.token,
-        ['additionalRoomFeatures']
-      )
-      setAditional(additionalRoomFeatures)
-    })()
-  }, [session])
+        ["additionalRoomFeatures"]
+      );
+      setAditional(additionalRoomFeatures);
+    })();
+  }, [session]);
 
   const formatFeature = (idx): string[] => {
-    const formatedItems = []
+    const formatedItems = [];
 
-    if (idx.length > 0 && idx[0]._id) return idx
+    if (idx.length > 0 && idx[0]?._id) return idx;
 
-    if (typeof idx === 'object')
+    if (typeof idx === "object")
       aditional.map((item) => {
-        const found = idx.find((itemToFind) => item._id === itemToFind)
-        if (found) formatedItems.push(item)
-      })
+        const found = idx.find((itemToFind) => item._id === itemToFind);
+        if (found) formatedItems.push(item);
+      });
     else
       aditional.map((item) => {
-        const found = idx.split(', ').find((att) => item.name === att)
-        if (found) formatedItems.push(item)
-      })
+        const found = idx.split(", ").find((att) => item.name === att);
+        if (found) formatedItems.push(item);
+      });
 
-    return formatedItems
-  }
+    return formatedItems;
+  };
 
   const formik = useFormik({
     initialValues: {
-      _id: data?._id || '',
+      _id: data?._id || "",
       availability: data?.availability || [],
-      type: data?.type || '',
-      bathroomLocation: data?.bathroomLocation || '',
-      bathType: data?.bathType || '',
-      bedType: data?.bedType || '',
-      floor: data?.floor || '',
+      type: data?.type || "",
+      bathroomLocation: data?.bathroomLocation || "",
+      bathType: data?.bathType || "",
+      bedType: data?.bedType || "",
+      floor: data?.floor || "",
       aditionalFeatures: data?.aditionalFeatures || [],
       photos: bedroomPictures,
+      roomNumber: thisRoomNumber,
     },
     validate: (data) => {
-      let errors: Partial<bedroomData> = {}
-      if (data.availability === '') {
-        errors.availability = 'Availability is required'
+      let errors: Partial<bedroomData> = {};
+      if (data.availability === "") {
+        errors.availability = "Availability is required";
       }
-      if (data.type === '') {
-        errors.type = 'Type is required'
+      if (data.type === "") {
+        errors.type = "Type is required";
       }
-      if (data.bathroomLocation === '') {
-        errors.bathroomLocation = 'Bathroom Location is required'
+      if (data.bathroomLocation === "") {
+        errors.bathroomLocation = "Bathroom Location is required";
       }
-      if (data.bathType === '') {
-        errors.bathType = 'Bath Type is required'
+      if (data.bathType === "") {
+        errors.bathType = "Bath Type is required";
       }
-      if (data.floor === '') {
-        errors.floor = 'Floor is required'
+      if (data.floor === "") {
+        errors.floor = "Floor is required";
       }
-      return errors
+      return errors;
     },
     onSubmit: (formikData) =>
       onSubmit({
@@ -102,136 +129,146 @@ const BedroomModal: FC<Props> = ({
           photo: pic.src || pic.photo,
         })),
       }),
-  })
+  });
   const isFormFieldValid = (name) =>
-    !!(formik.touched[name] && formik.errors[name])
+    !!(formik.touched[name] && formik.errors[name]);
   const getFormErrorMessage = (name) => {
     return (
       isFormFieldValid(name) && (
-        <small className='p-error'>{formik.errors[name]}</small>
+        <small className="p-error">{formik.errors[name]}</small>
       )
-    )
-  }
+    );
+  };
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <InputContainer label='Photo'>
+      <input type="hidden" name="roomNumber" value={thisRoomNumber} />
+      <InputContainer label="Photo">
         <Gallery homeCase images={bedroomPictures} />
         {!data.type && (
           <span>Please, save the bedroom after upload pictures</span>
         )}
         <Button
           disabled={!data.type ? true : false}
-          style={{ width: 'fit-content' }}
-          type='button'
+          style={{ width: "fit-content" }}
+          type="button"
           label="Upload beedroom's pictures"
           onClick={() => setShowPicturesModal(true)}
         />
       </InputContainer>
+
       <InputContainer
-        label='Availability'
-        labelClass={classNames({ 'p-error': isFormFieldValid('availability') })}
+        label="Room number"
+        labelClass={classNames({ "p-error": isFormFieldValid("roomNumber") })}
+      >
+        <span>{data?.roomNumber || thisRoomNumber}</span>
+        {getFormErrorMessage("roomNumber")}
+      </InputContainer>
+
+      <InputContainer
+        label="Availability"
+        labelClass={classNames({ "p-error": isFormFieldValid("availability") })}
       >
         <AvailabilityPicker
           dates={formik.values.availability}
           setDates={formik.handleChange}
         />
-        {getFormErrorMessage('availability')}
+        {getFormErrorMessage("availability")}
       </InputContainer>
       <InputContainer
-        label='Type'
-        labelClass={classNames({ 'p-error': isFormFieldValid('type') })}
+        label="Type"
+        labelClass={classNames({ "p-error": isFormFieldValid("type") })}
       >
         <Dropdown
-          id='type'
+          id="type"
           options={Types}
-          placeholder='Type'
+          placeholder="Type"
           value={formik.values.type}
           onChange={formik.handleChange}
-          className={classNames({ 'p-invalid': isFormFieldValid('type') })}
+          className={classNames({ "p-invalid": isFormFieldValid("type") })}
         />
-        {getFormErrorMessage('bathroomLocation')}
+        {getFormErrorMessage("bathroomLocation")}
       </InputContainer>
       <InputContainer
-        label='Bathroom Location'
+        label="Bathroom Location"
         labelClass={classNames({
-          'p-error': isFormFieldValid('bathroomLocation'),
+          "p-error": isFormFieldValid("bathroomLocation"),
         })}
       >
         <Dropdown
-          id='bathroomLocation'
+          id="bathroomLocation"
           options={BathroomLocationTypes}
-          placeholder='Bathroom Location'
+          placeholder="Bathroom Location"
           value={formik.values.bathroomLocation}
           onChange={formik.handleChange}
           className={classNames({
-            'p-invalid': isFormFieldValid('bathroomLocation'),
+            "p-invalid": isFormFieldValid("bathroomLocation"),
           })}
         />
-        {getFormErrorMessage('bathroomLocation')}
+        {getFormErrorMessage("bathroomLocation")}
       </InputContainer>
       <InputContainer
-        label='Bath Type'
-        labelClass={classNames({ 'p-error': isFormFieldValid('bathType') })}
+        label="Bath Type"
+        labelClass={classNames({ "p-error": isFormFieldValid("bathType") })}
       >
         <Dropdown
-          id='bathType'
+          id="bathType"
           options={BathTypes}
-          placeholder='Bath Type'
+          placeholder="Bath Type"
           value={formik.values.bathType}
           onChange={formik.handleChange}
-          className={classNames({ 'p-invalid': isFormFieldValid('bathType') })}
+          className={classNames({ "p-invalid": isFormFieldValid("bathType") })}
         />
-        {getFormErrorMessage('bathType')}
+        {getFormErrorMessage("bathType")}
       </InputContainer>
       <InputContainer
-        label='Bed Type'
-        labelClass={classNames({ 'p-error': isFormFieldValid('bedType') })}
+        label="Bed Type"
+        labelClass={classNames({ "p-error": isFormFieldValid("bedType") })}
       >
         <Dropdown
-          id='bedType'
+          id="bedType"
           options={BedTypes}
-          placeholder='Bed Type'
+          placeholder="Bed Type"
           value={formik.values.bedType}
           onChange={formik.handleChange}
-          className={classNames({ 'p-invalid': isFormFieldValid('bedType') })}
+          className={classNames({ "p-invalid": isFormFieldValid("bedType") })}
         />
-        {getFormErrorMessage('bedType')}
+        {getFormErrorMessage("bedType")}
       </InputContainer>
       <InputContainer
-        label='Floor'
-        labelClass={classNames({ 'p-error': isFormFieldValid('floor') })}
+        label="Floor"
+        labelClass={classNames({ "p-error": isFormFieldValid("floor") })}
       >
         <Dropdown
-          id='floor'
+          id="floor"
           options={FloorTypes}
-          placeholder='Select the floor'
+          placeholder="Select the floor"
           value={formik.values.floor}
           onChange={formik.handleChange}
-          className={classNames({ 'p-invalid': isFormFieldValid('floor') })}
+          className={classNames({ "p-invalid": isFormFieldValid("floor") })}
         />
-        {getFormErrorMessage('floor')}
+        {getFormErrorMessage("floor")}
       </InputContainer>
       <InputContainer
-        label='Aditional Features'
+        label="Aditional Features"
         labelClass={classNames({
-          'p-error': isFormFieldValid('aditionalFeatures'),
+          "p-error": isFormFieldValid("aditionalFeatures"),
         })}
       >
         <MultiSelect
-          id='aditionalFeatures'
-          name='aditionalFeatures'
-          optionLabel='name'
+          id="aditionalFeatures"
+          name="aditionalFeatures"
+          optionLabel="name"
           options={aditional}
           value={formatFeature(formik.values.aditionalFeatures)}
-          selectedItemTemplate={(item) => (item ? `${item?.name}, ` : '')}
+          selectedItemTemplate={(item) => (item ? `${item?.name}, ` : "")}
           onChange={formik.handleChange}
-          placeholder='Select aditional features'
+          placeholder="Select aditional features"
         />
-        {getFormErrorMessage('aditionalFeatures')}
+        {getFormErrorMessage("aditionalFeatures")}
       </InputContainer>
-      <Button type='submit'>Save</Button>
+      <Button type="submit">Save</Button>
     </form>
-  )
-}
-export default BedroomModal
+  );
+};
+export default BedroomModal;
