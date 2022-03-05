ffbf558afcc8df68305b13a483a3158bbea21460
Ubaldo <sarcosas.dev@gmail.com>
2022-03-05 18:55:07 -0400

    changes in familyPictures form and image uploader

--- src/components/Families/modals/FamilyPicturesForm.tsx
+++ src/components/Families/modals/FamilyPicturesForm.tsx
@@ -1,95 +1,95 @@
-import React, { useState, useMemo, useContext, useEffect, useRef } from 'react'
-import axios from 'axios'
-import { useSession } from 'next-auth/client'
+import React, { useState, useMemo, useContext, useEffect, useRef } from "react";
+import axios from "axios";
+import { useSession } from "next-auth/client";
 //components
-import { Toast } from 'primereact/toast'
-import ImageUploader from 'components/UI/Molecules/ImageUploader'
+import { Toast } from "primereact/toast";
+import ImageUploader from "components/UI/Molecules/ImageUploader";
 //context
-import { FamilyContext } from 'context/FamilyContext'
-const msFamily = 'ms-fands/api/v1'
+import { FamilyContext } from "context/FamilyContext";
+const msFamily = "ms-fands/api/v1";
 
 const FamilyPicturesForm = ({ setVisible }) => {
-  const [pictures, setPictures] = useState([])
-  const [progress, setProgress] = useState(0)
-  const { family, getFamily } = useContext(FamilyContext)
-  const formData = useMemo(() => new FormData(), [])
-  const [session] = useSession()
-  const [isLoading, setIsloading] = useState(false)
-  const toast = useRef(null)
+  const [pictures, setPictures] = useState([]);
+  const [progress, setProgress] = useState(0);
+  const { family, getFamily } = useContext(FamilyContext);
+  const formData = useMemo(() => new FormData(), []);
+  const [session] = useSession();
+  const [isLoading, setIsloading] = useState(false);
+  const toast = useRef(null);
 
   const showSuccess = (msg) => {
     toast.current.show({
-      severity: 'success',
-      summary: 'Success Message',
+      severity: "success",
+      summary: "Success Message",
       detail: msg,
       life: 3000,
-    })
-  }
+    });
+  };
   const showError = () => {
     toast.current.show({
-      severity: 'error',
-      summary: 'Error Message',
-      detail: 'An error has ocurred',
+      severity: "error",
+      summary: "Error Message",
+      detail: "An error has ocurred",
       life: 3000,
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     setPictures(
       family.familyPictures
         .filter((picture) => picture !== null)
         .map((picture, index) => {
-          formData.append(`familyPictures[${index}][picture]`, picture.picture)
-          formData.append(`familyPictures[${index}][caption]`, picture.caption)
+          formData.append(`familyPictures[${index}][picture]`, picture.picture);
+          formData.append(`familyPictures[${index}][caption]`, picture.caption);
 
           return {
             src: picture.picture,
             caption: picture.caption,
             id: index,
-          }
+          };
         })
-    )
-  }, [family.familyPictures])
+    );
+  }, [family.familyPictures]);
 
   const submit = () => {
-    setIsloading(true)
-    if (pictures.length === 0) formData.append('familyPictures', '[]')
+    setIsloading(true);
+    if (pictures.length === 0) formData.append("familyPictures", "[]");
 
     axios({
       url: `${process.env.NEXT_PUBLIC_API_URL}/${msFamily}/admin/families/${family._id}`,
-      method: 'PUT',
+      method: "PUT",
       data: formData,
       onUploadProgress: (p) => setProgress((p.loaded / p.total) * 100),
       headers: {
-        'Content-Type': 'multipart/form-data',
+        "Content-Type": "multipart/form-data",
         Authorization: `Bearer ${session?.token}`,
       },
     })
       .then((res) => {
-        showSuccess('Family pictures successfully updated')
-        getFamily()
+        showSuccess("Family pictures successfully updated");
+        getFamily();
         setTimeout(() => {
-          setVisible(false)
-        }, 1500)
+          setVisible(false);
+        }, 1500);
       })
       .catch((err) => {
-        console.error(err)
-        showError()
+        console.error(err);
+        showError();
         setTimeout(() => {
-          setVisible(false)
-        }, 1500)
-      })
-  }
+          setVisible(false);
+        }, 1500);
+      });
+  };
 
   const onChangeHandler = (e) => {
     formData.append(
       `familyPictures[${pictures.length}][picture]`,
       e.target.files[0]
-    )
+    );
     formData.append(
       `familyPictures[${pictures.length}][caption]`,
       e.target.files[0]?.name
-    )
+    );
     setPictures([
       ...pictures,
       {
@@ -97,22 +97,22 @@ const FamilyPicturesForm = ({ setVisible }) => {
         caption: e.target.files[0]?.name,
         id: pictures.length,
       },
-    ])
-  }
+    ]);
+  };
 
   const handleDelete = (data) => {
-    const updatedData = pictures.filter((picture) => picture.id !== data.id)
+    const updatedData = pictures.filter((picture) => picture.id !== data.id);
 
-    formData.delete(`familyPictures[${data.id}][picture]`)
-    formData.delete(`familyPictures[${data.id}][caption]`)
+    formData.delete(`familyPictures[${data.id}][picture]`);
+    formData.delete(`familyPictures[${data.id}][caption]`);
 
-    setPictures(updatedData)
-  }
+    setPictures(updatedData);
+  };
   return (
     <>
       <ImageUploader
-        id='file'
-        name='file'
+        id="file"
+        name="file"
         pictures={pictures}
         setPictures={setPictures}
         loading={isLoading}
@@ -120,9 +120,10 @@ const FamilyPicturesForm = ({ setVisible }) => {
         onChange={onChangeHandler}
         onDelete={handleDelete}
         progress={progress}
+        handleCLoseModal={setVisible}
       />
       <Toast ref={toast} />
     </>
-  )
-}
-export default FamilyPicturesForm
+  );
+};
+export default FamilyPicturesForm;
--- src/components/Families/modals/HomePicturesModal.tsx
+++ src/components/Families/modals/HomePicturesModal.tsx
@@ -1,136 +1,136 @@
-import { useState, useMemo, useContext, useEffect, useRef } from 'react'
-import axios from 'axios'
-import { useSession } from 'next-auth/client'
+import { useState, useMemo, useContext, useEffect, useRef } from "react";
+import axios from "axios";
+import { useSession } from "next-auth/client";
 //components
-import ImageUploader from 'components/UI/Molecules/ImageUploader'
-import { Toast } from 'primereact/toast'
+import ImageUploader from "components/UI/Molecules/ImageUploader";
+import { Toast } from "primereact/toast";
 //context
-import { FamilyContext } from 'context/FamilyContext'
-const msFamily = 'ms-fands/api/v1'
-
+import { FamilyContext } from "context/FamilyContext";
+const msFamily = "ms-fands/api/v1";
+import FamiliesService from "services/Families";
 const HomePicturesForm = ({
   setVisible,
   pictures,
   homeCategory,
   setPictures,
 }) => {
-  const toast = useRef(null)
-  const formData = useMemo(() => new FormData(), [])
-  const { family, getFamily } = useContext(FamilyContext)
-  const [progress, setProgress] = useState(0)
-  const [actualIndex, setActualIndex] = useState(0)
-  const [session] = useSession()
-  const [isLoading, setIsloading] = useState(false)
+  const toast = useRef(null);
+  const formData = useMemo(() => new FormData(), []);
+  const { family, getFamily } = useContext(FamilyContext);
+  const [progress, setProgress] = useState(0);
+  const [actualIndex, setActualIndex] = useState(0);
+  const [session] = useSession();
+  const [isLoading, setIsloading] = useState(false);
 
   const showSuccess = (msg) => {
-    toast.current.show({
-      severity: 'success',
-      summary: 'Success Message',
+    toast.current?.show({
+      severity: "success",
+      summary: "Success Message",
       detail: msg,
       life: 3000,
-    })
-  }
+    });
+  };
   const showError = () => {
-    toast.current.show({
-      severity: 'error',
-      summary: 'Error Message',
-      detail: 'An error has ocurred',
+    toast.current?.show({
+      severity: "error",
+      summary: "Error Message",
+      detail: "An error has ocurred",
       life: 3000,
-    })
-  }
+    });
+  };
 
   const groupBy = (group, key: string) => {
     return group.reduce((acum: object, filter: object) => {
-      ;(acum[filter[key]] = acum[filter[key]] || []).push(filter)
-      return acum
-    }, {})
-  }
+      (acum[filter[key]] = acum[filter[key]] || []).push(filter);
+      return acum;
+    }, {});
+  };
 
   useEffect(() => {
     const pictures = groupBy(
       family.home?.photoGroups.filter((g) => g !== null),
-      'name'
-    )
-    let find = false
-    let actualIdx = 0
-    const pics = []
+      "name"
+    );
+    let find = false;
+    let actualIdx = 0;
+    const pics = [];
 
     Object.entries(pictures).forEach((obj: any, idx: number) => {
       if (obj[0] === homeCategory) {
-        find = true
-        actualIdx = idx
+        find = true;
+        actualIdx = idx;
       } else {
         if (!find) {
-          actualIdx = family.home?.photoGroups.length
+          actualIdx = family.home?.photoGroups.length;
           formData.append(
             `photoGroups[${family.home?.photoGroups.length}][name]`,
             homeCategory
-          )
-          find = true
+          );
+          find = true;
         }
       }
-      formData.append(`photoGroups[${idx}][name]`, obj[0])
+      formData.append(`photoGroups[${idx}][name]`, obj[0]);
       obj[1][0].photos.forEach((pic, index) => {
         formData.append(
           `photoGroups[${idx}][photos][${index}][photo]`,
           pic.src || pic.photo
-        )
+        );
         if (obj[0] === homeCategory)
           pics.push({
             src: pic.photo || pic.src,
             alt: pic.caption || pic._id,
             id: `${index}`,
-          })
-      })
-    })
+          });
+      });
+    });
 
     family.home?.photoGroups.length === 0 &&
-      formData.append('photoGroups[0][name]', homeCategory)
+      formData.append("photoGroups[0][name]", homeCategory);
 
-    setActualIndex(actualIdx)
-    setPictures(pics)
-  }, [family.home?.photoGroups])
+    setActualIndex(actualIdx);
+    setPictures(pics);
+  }, [family.home?.photoGroups]);
 
   const submit = () => {
-    setIsloading(true)
+    setIsloading(true);
     if (pictures.length === 0)
-      formData.append(`photoGroups[${actualIndex}][photos]`, '[]')
+      formData.append(`photoGroups[${actualIndex}][photos]`, "[]");
 
     axios({
       url: `${process.env.NEXT_PUBLIC_API_URL}/${msFamily}/admin/families/${family._id}/home`,
-      method: 'PUT',
+      method: "PUT",
       data: formData,
       onUploadProgress: (p) => {
-        setProgress((p.loaded / p.total) * 100)
+        setProgress((p.loaded / p.total) * 100);
       },
       headers: {
-        'Content-Type': 'multipart/form-data',
+        "Content-Type": "multipart/form-data",
         Authorization: `Bearer ${session?.token}`,
       },
     })
       .then((res) => {
-        showSuccess('Home pictures successfully updated')
-        getFamily()
-        setTimeout(() => setVisible(false), 1500)
+        showSuccess("Home pictures successfully updated");
+        getFamily();
+        setTimeout(() => setVisible(false), 1500);
       })
       .catch((err) => {
-        console.error(err)
-        showError()
-        setTimeout(() => setVisible(false), 1500)
-      })
-  }
+        console.error(err);
+        showError();
+        setTimeout(() => setVisible(false), 1500);
+      });
+  };
 
   const onChangeHandler = (e) => {
-    console.log(e.target.files[0])
+    console.log(e.target.files[0]);
     if (
-      e.target?.files[0].type === 'image/jpeg' ||
-      e.target?.files[0].type === 'image/pjpeg' ||
-      e.target?.files[0].type === 'image/png'
+      e.target?.files[0].type === "image/jpeg" ||
+      e.target?.files[0].type === "image/pjpeg" ||
+      e.target?.files[0].type === "image/png"
     ) {
       formData.append(
         `photoGroups[${actualIndex}][photos][${pictures?.length || 0}][photo]`,
         e.target.files[0]
-      )
+      );
 
       setPictures([
         ...(pictures || []),
@@ -139,41 +139,59 @@ const HomePicturesForm = ({
           caption: e.target.files[0]?.name,
           id: pictures?.length || 0,
         },
-      ])
+      ]);
     }
-  }
+  };
 
-  const handleDelete = (data) => {
+  const handleDelete = async (data) => {
     const updatedData = [
       ...pictures
         .filter((picture) => picture.id !== data.id)
         .map((picture, index) => ({ ...picture, id: index })),
-    ]
+    ];
+    console.log(data, homeCategory);
 
-    pictures.forEach((_, index) => {
-      formData.delete(`photoGroups[${actualIndex}][photos][${index}][photo]`)
-      formData.delete(`photoGroups[${actualIndex}][photos][${index}][caption]`)
-    })
+    if (updatedData.length === 0) {
+      await FamiliesService.updateFamilyHome(session?.token, family._id, {
+        photoGroups: [],
+      });
+      await FamiliesService.updateFamilyHome(session?.token, family._id, {
+        photoGroups: [
+          ...family.home.photoGroups.filter((pgf) => pgf.name !== homeCategory),
+          {
+            ...family.home.photoGroups.find((pg) => pg.name === homeCategory),
+            photos: [],
+          },
+        ],
+      });
+    } else {
+      pictures.forEach((_, index) => {
+        formData.delete(`photoGroups[${actualIndex}][photos][${index}][photo]`);
+        formData.delete(
+          `photoGroups[${actualIndex}][photos][${index}][caption]`
+        );
+      });
 
-    updatedData.forEach((picture) => {
-      formData.append(
-        `photoGroups[${actualIndex}][photos][${picture.id}][photo]`,
-        picture.src
-      )
-      formData.append(
-        `photoGroups[${actualIndex}][photos][${picture.id}][caption]`,
-        picture.alt
-      )
-    })
+      updatedData.forEach((picture) => {
+        formData.append(
+          `photoGroups[${actualIndex}][photos][${picture.id}][photo]`,
+          picture.src
+        );
+        formData.append(
+          `photoGroups[${actualIndex}][photos][${picture.id}][caption]`,
+          picture.alt
+        );
+      });
+    }
 
-    setPictures(updatedData)
-  }
+    setPictures(updatedData);
+  };
 
   return (
     <>
       <ImageUploader
-        id='file'
-        name='file'
+        id="file"
+        name="file"
         pictures={pictures}
         category={homeCategory}
         setPictures={setPictures}
@@ -182,9 +200,10 @@ const HomePicturesForm = ({
         onChange={onChangeHandler}
         onDelete={handleDelete}
         progress={progress}
+        handleCLoseModal={setVisible}
       />
       <Toast ref={toast} />
     </>
-  )
-}
-export default HomePicturesForm
+  );
+};
+export default HomePicturesForm;
