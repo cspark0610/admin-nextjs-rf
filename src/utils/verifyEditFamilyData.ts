540874f9e0ae114b753070b0830e96ff9216e715
Ubaldo <sarcosas.dev@gmail.com>
2022-03-11 15:15:09 -0400

    fixed birth on update

--- src/utils/verifyEditFamilyData.ts
+++ src/utils/verifyEditFamilyData.ts
@@ -1,4 +1,4 @@
-import type { Family } from 'context/RegisterFamilyContext'
+import type { Family } from "context/RegisterFamilyContext";
 
 export const verifyEditFamilyData = (
   family: Family | any[] | any,
@@ -6,131 +6,132 @@ export const verifyEditFamilyData = (
 ): string[] => {
   switch (actualStep) {
     case 0:
-      return verifyUserData(family as Family)
+      return verifyUserData(family as Family);
     case 1:
-      return verifyHostData(family)
+      return verifyHostData(family);
     case 2:
-      return verifyFamilyData(family as Family)
+      return verifyFamilyData(family as Family);
     case 3:
-      return verifyWelcomeStudentGender(family as any[])
+      return verifyWelcomeStudentGender(family as any[]);
     case 4:
-      return verifyLodgingData(family)
+      return verifyLodgingData(family);
     case 5:
-      return verifyHomeData(family)
+      return verifyHomeData(family);
     case 6:
-      return verifyTenansError(family)
+      return verifyTenansError(family);
     default:
-      return []
+      return [];
   }
-}
+};
 
 const verifyUserData = ({ user }: Family): string[] => {
-  const verify = []
+  const verify = [];
 
-  if (!user.first_name) verify.push('First name')
-  if (!user.last_name) verify.push('Last name')
-  if (!user.email) verify.push('Email')
-  else if (!user.email.includes('@')) verify.push('Email')
-  if (!user.password) verify.push('Password')
-  if (!user.confirmPass) verify.push('Confirm password')
+  if (!user.first_name) verify.push("First name");
+  if (!user.last_name) verify.push("Last name");
+  if (!user.email) verify.push("Email");
+  else if (!user.email.includes("@")) verify.push("Email");
+  if (!user.password) verify.push("Password");
+  if (!user.confirmPass) verify.push("Confirm password");
 
-  return verify
-}
+  return verify;
+};
 
 const verifyHostData = (mainMembers: any): string[] => {
-  const verify = []
-  if (mainMembers.length === 0) verify.push('Main members')
+  const verify = [];
+  if (mainMembers.length === 0) verify.push("Main members");
   else
     mainMembers.map((member, idx) => {
-      if (!member.firstName) verify.push(`Member ${idx + 1}: First name`)
-      if (!member.lastName) verify.push(`Member ${idx + 1}: Last name`)
-      if (!member.email) verify.push(`Member ${idx + 1}: Email`)
-      else if (!member.email.includes('@')) verify.push('Email')
-      if (!member.occupation && !member.occupationFreeComment) verify.push(`Member ${idx + 1}: Occupation`)
-      if (!member.gender) verify.push(`Member ${idx + 1}: Gender`)
-      if (!member.birthDate) verify.push(`Member ${idx + 1}: Date of birth`)
+      if (!member.firstName) verify.push(`Member ${idx + 1}: First name`);
+      if (!member.lastName) verify.push(`Member ${idx + 1}: Last name`);
+      if (!member.email) verify.push(`Member ${idx + 1}: Email`);
+      else if (!member.email.includes("@")) verify.push("Email");
+      if (!member.occupation && !member.occupationFreeComment)
+        verify.push(`Member ${idx + 1}: Occupation`);
+      if (!member.gender) verify.push(`Member ${idx + 1}: Gender`);
+      // if (!member.birthDate) verify.push(`Member ${idx + 1}: Date of birth`)
       if (!member.mainLanguagesSpokenAtHome)
-        verify.push(`Member ${idx + 1}: Main languages spoken at home`)
+        verify.push(`Member ${idx + 1}: Main languages spoken at home`);
       if (!member.spokenLanguages)
-        verify.push(`Member ${idx + 1}: spoken languages`)
+        verify.push(`Member ${idx + 1}: spoken languages`);
       if (!member.cellPhoneNumber)
-        verify.push(`Member ${idx + 1}: Cellphone number`)
-    })
+        verify.push(`Member ${idx + 1}: Cellphone number`);
+    });
 
-  return verify
-}
+  return verify;
+};
 
 const verifyFamilyData = ({ familyMembers }: Family): string[] => {
-  const verify = []
+  const verify = [];
   if (familyMembers.length > 0) {
     familyMembers.map((member, idx) => {
-      if (!member.firstName) verify.push(`Member ${idx + 1}: First name`)
-      if (!member.lastName) verify.push(`Member ${idx + 1}: Last name`)
-      if (!member.gender) verify.push(`Member ${idx + 1}: Gender`)
+      if (!member.firstName) verify.push(`Member ${idx + 1}: First name`);
+      if (!member.lastName) verify.push(`Member ${idx + 1}: Last name`);
+      if (!member.gender) verify.push(`Member ${idx + 1}: Gender`);
       if (!member.familyRelationship)
-        verify.push(`Member ${idx + 1}: Relationship with primary host`)
-      if (!member.birthDate) verify.push(`Member ${idx + 1}: Date of birth`)
+        verify.push(`Member ${idx + 1}: Relationship with primary host`);
+      //if (!member.birthDate) verify.push(`Member ${idx + 1}: Date of birth`)
       if (!member.spokenLanguages)
-        verify.push(`Member ${idx + 1}: Spoken languages`)
-      if (!member.situation) verify.push(`Member ${idx + 1}: situation`)
-    })
+        verify.push(`Member ${idx + 1}: Spoken languages`);
+      if (!member.situation) verify.push(`Member ${idx + 1}: situation`);
+    });
   }
-  return verify
-}
+  return verify;
+};
 
 const verifyWelcomeStudentGender = (
   welcomeStudentGenders: string[]
 ): string[] => {
-  const verify = []
-  if (welcomeStudentGenders.length === 0) verify.push('Our family welcomes')
+  const verify = [];
+  if (welcomeStudentGenders.length === 0) verify.push("Our family welcomes");
 
-  return verify
-}
+  return verify;
+};
 
 const verifyLodgingData = (home: any): string[] => {
-  const verify = []
+  const verify = [];
 
-  if (!home?.country) verify.push('Country')
-  if (!home?.province) verify.push('Province')
-  if (!home?.city && !home?.cityFreeComment) verify.push('City')
-  if (!home?.postalCode) verify.push('Postal code')
-  if (!home?.address) verify.push('Address')
+  if (!home?.country) verify.push("Country");
+  if (!home?.province) verify.push("Province");
+  if (!home?.city && !home?.cityFreeComment) verify.push("City");
+  if (!home?.postalCode) verify.push("Postal code");
+  if (!home?.address) verify.push("Address");
 
-  return verify
-}
+  return verify;
+};
 
 const verifyHomeData = (home: any): string[] => {
-  const verify = []
+  const verify = [];
 
-  if (!home?.homeType) verify.push('House type')
-  if (home?.houseRooms?.length === 0) verify.push('Inside')
-  if (home?.services?.length === 0) verify.push('Household amenities')
+  if (!home?.homeType) verify.push("House type");
+  if (home?.houseRooms?.length === 0) verify.push("Inside");
+  if (home?.services?.length === 0) verify.push("Household amenities");
   if (!home.studentRooms || home?.studentRooms.length === 0)
-    verify.push('Student Rooms')
+    verify.push("Student Rooms");
   else
     home?.studentRooms.map((room, idx) => {
-      if (!room.type) verify.push(`Room ${idx + 1}: Room Type`)
-      if (!room.bathType) verify.push(`Room ${idx + 1}: Bath Type`)
-      if (!room.bedType) verify.push(`Room ${idx + 1}: Bed Type`)
-      if (!room.floor) verify.push(`Room ${idx + 1}: Room location`)
-    })
+      if (!room.type) verify.push(`Room ${idx + 1}: Room Type`);
+      if (!room.bathType) verify.push(`Room ${idx + 1}: Bath Type`);
+      if (!room.bedType) verify.push(`Room ${idx + 1}: Bed Type`);
+      if (!room.floor) verify.push(`Room ${idx + 1}: Room location`);
+    });
 
-  return verify
-}
+  return verify;
+};
 
 const verifyTenansError = (family: any): string[] => {
-  const verify = []
+  const verify = [];
   if (family.haveTenants === false && family.tenants.length > 0)
     verify.push(
-      'You have registered a tenant, you must check the option tenants, or delete the tenants'
-    )
+      "You have registered a tenant, you must check the option tenants, or delete the tenants"
+    );
   if (
     family.haveExternalStudents === false &&
     family.externalStudents.length > 0
   )
     verify.push(
-      'You have registered an external student, you must check the option external students, or delete the external student'
-    )
+      "You have registered an external student, you must check the option external students, or delete the external student"
+    );
 
-  return verify
-}
+  return verify;
+};
