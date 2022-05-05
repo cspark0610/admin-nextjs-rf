850606ad908f12ad0d370d51158088c093c5031a
josejmv <josejmvasquez@gmail.com>
2022-05-05 17:47:53 -0400

    [hotfix] change auth service

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 // main tools
-import { getSession, useSession } from 'next-auth/react'
+import { getSession } from 'next-auth/react'
 import Image from 'next/image'
 
 // components
@@ -11,25 +11,19 @@ import classes from 'styles/Home/Home.module.scss'
 // types
 import { GetServerSidePropsContext, NextPage } from 'next'
 
-const HomePage: NextPage = () => {
-  const { data, status } = useSession()
-
-  console.log({ data, status })
-
-  return (
-    <Layout noPadding>
-      <div className={classes.home}>
-        <Image
-          width={500}
-          height={500}
-          alt='logo redleaf'
-          className={classes.img}
-          src='/assets/logo-redleaf.svg'
-        />
-      </div>
-    </Layout>
-  )
-}
+const HomePage: NextPage = () => (
+  <Layout noPadding>
+    <div className={classes.home}>
+      <Image
+        width={500}
+        height={500}
+        alt='logo redleaf'
+        className={classes.img}
+        src='/assets/logo-redleaf.svg'
+      />
+    </div>
+  </Layout>
+)
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const session = await getSession(ctx)
